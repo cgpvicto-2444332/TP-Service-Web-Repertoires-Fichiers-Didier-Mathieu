03094714cd12a7bac5f08528ce481c4334011c44
Didier Mathieu <dmathieu2007@gmail.com>
2026-04-26 12:31:52 -0400

    ajout d'insertions dans le script de la bd

--- creation_bd.sql
+++ creation_bd.sql
@@ -3,7 +3,7 @@ drop table if exists public.livres;
 drop table if exists public.bibliotheque;
 
 create table public.bibliotheque (
-    id INTEGER primary key,
+    id SERIAL primary key,
     nom VARCHAR(100),
     courriel VARCHAR(255),
     cle_api VARCHAR(30),
@@ -11,7 +11,7 @@ create table public.bibliotheque (
 );
 
 create table public.livres (
-	id INTEGER primary key,
+	id SERIAL primary key,
 	bibliotheque_id INTEGER,
 	titre VARCHAR(100),
 	auteur VARCHAR(100),
@@ -22,9 +22,31 @@ create table public.livres (
 );
 
 create table public.prets (
-	id INTEGER primary key,
+	id SERIAL primary key,
 	livre_id INTEGER,
 	emprunteur VARCHAR(100),
 	date_retour DATE,
 	foreign key (livre_id) references public.livres(id)
 );
+
+/**********************************************************************/
+/***************************** Insertion ******************************/
+/**********************************************************************/
+
+/***************** Bibliothèques *****************/
+insert into public.bibliotheque (nom, courriel, cle_api, password) values
+('Bibliothèque Charles-Édouard-Mailhot', 'bibliotheque@victoriaville.ca', '1234', 'mdp123'),
+('Bibliothèque Alcide-Fleury', 'bibliotheque@victoriaville.ca', '5678', 'mdp456');
+
+/***************** Livres *****************/
+insert into public.livres (bibliotheque_id, titre, auteur, isbn, date_ajout, disponible) values
+(1, 'Walking dead. 32, La fin du voyage', 'Robert Kirkman', '9782413016816', '2019-01-01', true),
+(1, 'Raconte-moi Marc-André Fleury', 'Frédéric Daigle', '9782897541675', '2019-02-15', false),
+(2, 'Raconte-moi Carey Price', 'Jean-François Chaumont', '9782924025949', '2015-03-05', true),
+(2, 'Ne jamais trembler', 'Stephen King', '9782226501875', '2016-03-20', false);
+
+/***************** Prêts *****************/
+insert into public.prets (livre_id, emprunteur, date_retour) values
+(2, 'Jean Dupont', '2024-05-01'),
+(4, 'Marie Tremblay', '2024-05-10');
+
