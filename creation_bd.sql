5679bc4c26ceaa71e0cc784f8f4649b92587ceb5
Didier Mathieu <dmathieu2007@gmail.com>
2026-04-26 16:56:21 -0400

    modification des insertions dans le script

--- creation_bd.sql
+++ creation_bd.sql
@@ -18,6 +18,7 @@ create table public.livres (
 	isbn VARCHAR(20),
 	date_ajout DATE,
 	disponible BOOLEAN,
+	description VARCHAR(255),
 	foreign key (bibliotheque_id) references public.bibliotheque(id)
 );
 
@@ -39,14 +40,15 @@ insert into public.bibliotheque (nom, courriel, cle_api, password) values
 ('Bibliothèque Alcide-Fleury', 'bibliotheque@victoriaville.ca', '5678', 'mdp456');
 
 /***************** Livres *****************/
-insert into public.livres (bibliotheque_id, titre, auteur, isbn, date_ajout, disponible) values
-(1, 'Walking dead. 32, La fin du voyage', 'Robert Kirkman', '9782413016816', '2019-01-01', true),
-(1, 'Raconte-moi Marc-André Fleury', 'Frédéric Daigle', '9782897541675', '2019-02-15', false),
-(2, 'Raconte-moi Carey Price', 'Jean-François Chaumont', '9782924025949', '2015-03-05', true),
-(2, 'Ne jamais trembler', 'Stephen King', '9782226501875', '2016-03-20', false);
+/* Les données des livres proviennent du site : https://biblio.victoriaville.ca/ */
+insert into public.livres (bibliotheque_id, titre, auteur, isbn, date_ajout, disponible, description) values
+(1, 'Walking dead. 32, La fin du voyage', 'Robert Kirkman', '9782413016816', '2019-01-01', false, '149 pages : principalement des illustrations (certaines en couleur) ; 26 cm'),
+(1, 'Raconte-moi Marc-André Fleury', 'Frédéric Daigle', '9782897541675', '2019-02-15', true, ''),
+(2, 'Raconte-moi Carey Price', 'Jean-François Chaumont', '9782924025949', '2015-03-05', true, '139 pages : illustrations ; 19 cm'),
+(2, 'Ne jamais trembler', 'Stephen King', '9782226501875', '2016-03-20', false, '');
 
 /***************** Prêts *****************/
 insert into public.prets (livre_id, emprunteur, date_retour) values
-(2, 'Jean Dupont', '2024-05-01'),
-(4, 'Marie Tremblay', '2024-05-10');
+(1, 'Jean Dupont', '2024-05-01'),
+(4, 'Marie Tremblay', null);
 
